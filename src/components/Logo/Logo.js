0c61c15a3039e58dad6538113f137bce117c5501
BLu <benny@bennylu.com>
2021-04-14 18:03:33 -0700

    Changed logo and some text

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import IconLogo from './IconLogo';
+import LogoImage from './LogoImage.png';
 import css from './Logo.module.css';
+import config from '../../config';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
   const mobileClasses = classNames(css.logoMobile, className);
-
   // If you want to use image instead of svg as a logo you can use the following code.
   // Also, remember to import the image as LogoImage here.
   // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
-
-  return (
-    <IconLogo
-      className={format === 'desktop' ? className : mobileClasses}
-      format={format}
-      {...rest}
-    />
-  );
+    if (format === 'desktop') {
+      return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+    }
+    return <IconLogo className={mobileClasses} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
